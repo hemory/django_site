61891ebf1bd1fa29ed0050335c8e79a9adb836e4
Hemory Phifer <hemoryphifer@hemorys-air.lan>
2022-02-05 22:51:19 -0500

    added template for index page and a new css

--- meetups/views.py
+++ meetups/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'meetups/index.html')
+    meetups = [
+        {
+            'title': 'A First Meetup',
+            'location': 'New York',
+            'slug': 'a-first-meetup'
+        },
+        {
+            'title': 'A Second Meetup',
+            'location': 'Michigan',
+            'slug': 'a-second-meetup'
+        }
+    ]
+    return render(request, 'meetups/index.html', {
+        'show_meetups': True,
+        'meetups': meetups
+    })
